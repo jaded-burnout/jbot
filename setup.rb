bad110d823c10e51ecb56110528c1a3afba0de98
Jaded Burnout <jaded.burnout69@gmail.com>
2019-01-12 14:34:44 +0000

    Create an `app` directory

--- setup.rb
+++ setup.rb
@@ -1,4 +1,9 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), "lib"))
+%w[
+  app
+  lib
+].each do |directory|
+  $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), directory))
+end
 
 require "rubygems"
 require "bundler/setup"
